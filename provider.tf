4c133bc3dc042fc50261d77a7cc7ef38debc4335
Gonçalo Almeida <goncalo.almeida@log.pt>
2024-08-24 11:17:05 +0000

    fmt terraform code

--- provider.tf
+++ provider.tf
@@ -1,20 +1,20 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "5.42.0"
     }
   }
 
   backend "gcs" {
-    bucket  = "cn-poc-terraform-remote-backend"
-    prefix  = "terraform/state"
+    bucket = "cn-poc-terraform-remote-backend"
+    prefix = "terraform/state"
   }
 }
 
 
 provider "google" {
   # Configuration options
-  project     = "log-gdc-poc"
-  region      = "europe-west3"
+  project = "log-gdc-poc"
+  region  = "europe-west3"
 }
