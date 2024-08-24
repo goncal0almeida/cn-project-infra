09c2f2fe899289cbf5d21a4dbca1c2597460f295
Gonçalo Almeida <goncalo.almeida@log.pt>
2024-08-24 11:02:44 +0000

    Initial commit

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.42.0"
+    }
+  }
+
+  backend "gcs" {
+    bucket  = "cn-poc-terraform-remote-backend"
+    prefix  = "terraform/state"
+  }
+}
+
+
+provider "google" {
+  # Configuration options
+  project     = "log-gdc-poc"
+  region      = "europe-west3"
+}
