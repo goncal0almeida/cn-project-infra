6209a69db3b83e878cb4b12a45060f4220d9ce64
Gonçalo Almeida <goncalo.almeida@log.pt>
2024-09-01 17:09:25 +0100

    uncommented infra

--- main.tf
+++ main.tf
@@ -1,58 +1,58 @@
-# resource "google_cloud_run_v2_service" "default" {
-#   name     = "test-service"
-#   location = var.location
-#   ingress  = "INGRESS_TRAFFIC_ALL"
-#
-#   template {
-#     containers {
-#       image = "us-docker.pkg.dev/cloudrun/container/hello"
-#     }
-#   }
-#
-#   lifecycle {
-#     ignore_changes = [
-#       template["image"],
-#     ]
-#   }
-# }
-#
-# resource "google_cloud_run_v2_service_iam_member" "member" {
-#   location = var.location
-#   name     = google_cloud_run_v2_service.default.name
-#   role     = "roles/run.invoker"
-#   member   = "allUsers"
-# }
-#
-# resource "google_compute_region_network_endpoint_group" "default" {
-#   name                  = "cloudrun-neg"
-#   network_endpoint_type = "SERVERLESS"
-#   region                = var.location
-#   cloud_run {
-#     service = google_cloud_run_v2_service.default.name
-#   }
-# }
-#
-# module "lb-http" {
-#   source  = "GoogleCloudPlatform/lb-http/google//modules/serverless_negs"
-#   version = "~> 9.0"
-#   project = var.project_id
-#   name    = "my-lb"
-#
-#   backends = {
-#     default = {
-#       groups = [
-#         {
-#         group = google_compute_region_network_endpoint_group.default.id }
-#       ]
-#
-#
-#       log_config = {
-#         enable = false
-#       }
-#
-#       iap_config = {
-#         enable = false
-#       }
-#     }
-#   }
-# }
+resource "google_cloud_run_v2_service" "default" {
+  name     = "test-service"
+  location = var.location
+  ingress  = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      template["image"],
+    ]
+  }
+}
+
+resource "google_cloud_run_v2_service_iam_member" "member" {
+  location = var.location
+  name     = google_cloud_run_v2_service.default.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
+resource "google_compute_region_network_endpoint_group" "default" {
+  name                  = "cloudrun-neg"
+  network_endpoint_type = "SERVERLESS"
+  region                = var.location
+  cloud_run {
+    service = google_cloud_run_v2_service.default.name
+  }
+}
+
+module "lb-http" {
+  source  = "GoogleCloudPlatform/lb-http/google//modules/serverless_negs"
+  version = "~> 9.0"
+  project = var.project_id
+  name    = "my-lb"
+
+  backends = {
+    default = {
+      groups = [
+        {
+        group = google_compute_region_network_endpoint_group.default.id }
+      ]
+
+
+      log_config = {
+        enable = false
+      }
+
+      iap_config = {
+        enable = false
+      }
+    }
+  }
+}
